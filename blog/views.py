05201ea17cb2a8963b42d6dc067b2ab43d365980
kimbaleum <kimbaleum@likelion.org>
2020-05-09 16:50:35 +0900

    blog proj

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Blog
+
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs' : blogs})
