5b4606673cd40fcc1a44775735d210729adb1ae2
kimbaleum <kimbaleum@likelion.org>
2020-05-23 20:32:46 +0900

    blog portfoilo second commit

--- blog/views.py
+++ blog/views.py
@@ -19,8 +19,8 @@ def new(request):
 def create(request):
     blog = Blog()
     blog.title = request.GET['title']
-    blog.body = request.GET['title']
+    blog.body = request.GET['body']
     blog.pub_date = timezone.datetime.now()
-    blog.save
+    blog.save()
     return redirect('/blog/'+str(blog.id))
 
